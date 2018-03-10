cfed392888ab4490866c2818ba28c8c1cc9d9de4
Vlad Holubiev <golubev.vld@gmail.com>
2018-03-10 16:44:45 +0200

    feat: parse topic of news article

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const _ = require('lodash');
 const {parse} = require('date-fns');
 const {getPageHTML} = require('./fetch');
 
+const dateRegex = /\d+-\d+-\d+, \d+:\d+/;
+
 module.exports.parsePage = async function parsePage(pageNumber) {
   const html = await getPageHTML(`http://www.tneu.edu.ua/news/page/${pageNumber}`);
   const $ = cheerio.load(html);
@@ -15,10 +17,11 @@ module.exports.parsePage = async function parsePage(pageNumber) {
     const title = article.find('h4').text();
     const description = normalizeSpace(article.find('.timg').text());
     const subtitle = article.find('.highlight').text();
-    const dateRaw = _.head(subtitle.match(/\d+-\d+-\d+, \d+:\d+/));
+    const dateRaw = _.head(subtitle.match(dateRegex));
     const publishedAt = parse(dateRaw);
+    const topic = normalizeSpace(_.last(subtitle.split(dateRegex)));
 
-    news.push({title, description, publishedAt});
+    news.push({title, description, publishedAt, topic});
   });
 
   return news;
